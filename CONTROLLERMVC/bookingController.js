26acbcaeed90a29c849dc24c6bd95ae60eee3a4f
alagammaiL <alagulakshmanan10398@gmail.com>
2025-06-18 09:07:57 +0530

    constructEvents fix

--- CONTROLLERMVC/bookingController.js
+++ CONTROLLERMVC/bookingController.js
@@ -64,7 +64,7 @@ exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    event = constructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET,
