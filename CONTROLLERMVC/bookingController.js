855c6a7569651f9f5ed546caba5c7b10dcc1f974
alagammaiL <alagulakshmanan10398@gmail.com>
2025-06-18 06:38:31 +0530

    smallbug

--- CONTROLLERMVC/bookingController.js
+++ CONTROLLERMVC/bookingController.js
@@ -21,7 +21,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     payment_method_types: ['card'],
     mode: 'payment', // ✅ REQUIRED when using price_data
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/tour`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tour`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
